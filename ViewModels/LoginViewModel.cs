445b11270ab671f036f90b63dd760cea45632e69
Danny Francisco Castillo Solano <93727378+Dan4541@users.noreply.github.com>
2025-12-12 18:31:37 -0600

    Login and Register processes from the controller implemented on the views

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -7,12 +7,12 @@ namespace DnTech_Ecommerce.ViewModels
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email format")]
         [Display(Name = "Email")]
-        public string Email { get; set; } = string.Empty;
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
-        public string Password { get; set; } = string.Empty;
+        public string? Password { get; set; }
 
         [Display(Name = "Remember Me")]
         public bool RememberMe { get; set; }
