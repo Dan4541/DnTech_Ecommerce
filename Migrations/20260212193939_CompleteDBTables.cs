254c3bab11a8c036e7944dcc202265e4db2ae8f2
Danny Castillo Solano <newdan049@gmail.com>
2026-02-12 15:24:32 -0600

    Basic config implemented again for the OS change

---
+++ Migrations/20260212193939_CompleteDBTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DnTech_Ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompleteDBTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
